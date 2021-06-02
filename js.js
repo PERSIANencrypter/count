5fbe9c289f54f08f849527cb5336c7c63bc0c457
PERSIANencrypter <79386552+PERSIANencrypter@users.noreply.github.com>
2021-06-02 16:33:56 +0430

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+function plus(){
+  
+  var addad = document.getElementById("mall");
+  addad.innerHTML++
+}
+function manfi(){
+  var mangi = document.getElementById("mall");
+  mangi.innerHTML--
+}
+function pak(){
+  document.getElementById("mall").innerHTML=0
+}
